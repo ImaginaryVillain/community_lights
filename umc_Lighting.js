6a6a8a55fa1a96c76b2bb79c5ea726c32666d81c
Aesica <aesica@gmail.com>
2020-12-01 13:38:17 -0800

    Resynch with ImaginaryVillain/community_lights ver

--- umc_Lighting.js
+++ umc_Lighting.js
@@ -1,15 +1,15 @@
 //=============================================================================
 // Community Plugins - Ultra Mini Lighting system
 // umc_Lighting.js
-// Version: 1.45
+// Version: 1.46
 /*=============================================================================
 Forked from Terrax Lighting
 =============================================================================*/
 var umc = umc || {};
 umc.Lighting = umc.Lighting || {};
-umc.Lighting.version = 1.45;
+umc.Lighting.version = 1.46;
 /*:
-* @plugindesc v1.2 The smallest, fastest lighting plugin!
+* @plugindesc v1.46 The smallest, fastest lighting plugin!
 * @author Terrax, iVillain, Aesica
 *
 * @param Lights Active Radius
@@ -48,47 +48,22 @@ umc.Lighting.version = 1.45;
 * - Light		
 * - radius      100, 250, etc
 * - color       #ffffff, #ff0000, etc
+*
+* --------------------------------------------------------------------------
+* Lights Active Radius
+* --------------------------------------------------------------------------
+* This allows you to decide how far away from the player lights are active,
+* anything beyond this range will not light up until the player gets
+* closer to it.
+* 
+* It can be changed in the plugin parameters, or using the script call...
+*
+* $gameVariables.SetActiveRadius(#)
+*
+* ....where # is the max distance you want in tiles.
 */
 
-umc.Lighting = {
-    event_note: [],
-    event_id: [],
-    event_stacknumber: [],
-    event_eventcount: 0,
-    parameters: PluginManager.parameters('umc_Lighting'),
-    oldmap: 0,
-    note_command: 0
-};
-
-umc.Lighting.getTag = function () {
-    let note = this.note;
-    let result = note.trim();
-    return result;
-};
-
-umc.Lighting.ReloadMapEvents = function () {
-    //**********************fill up new map-array *************************
-    this.event_note = [];
-    this.event_id = [];
-    this.event_stacknumber = [];
-    this.event_eventcount = $gameMap.events().length;
-
-    for (let i = 0, n = this.event_eventcount; i < n; i++) {
-        if ($gameMap.events()[i]) {
-            if ($gameMap.events()[i].event()) {
-                let note = this.getTag.call($gameMap.events()[i].event());
-                let note_args = note.split(" ");
-                umc.Lighting.note_command = note_args.shift().toLowerCase();
-
-                if (umc.Lighting.note_command == "light") {
-                    this.event_note.push(note);
-                    this.event_id.push($gameMap.events()[i]._eventId);
-                    this.event_stacknumber.push(i);
-                }
-            }
-        }
-    }
-};
+umc.Lighting.parameters = PluginManager.parameters('umc_Lighting');
 
 // *******************  NORMAL LIGHT SHAPE ***********************************
 // Fill gradient circle
@@ -129,14 +104,11 @@ umc.Lighting.tint = function (color) {
     $gameVariables.SetTint(color);
 };
 
-umc.Lighting.hexToRgb = function (hex) {
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    result = result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-    return result;
+Game_Variables.prototype.SetActiveRadius = function (value) {
+    this._Player_Light_Radius = value;
+};
+Game_Variables.prototype.GetActiveRadius = function () {
+    return this._Player_Light_Radius || Number(umc.Lighting.parameters['Lights Active Radius']);
 };
 
 Game_Variables.prototype.SetTint = function (value) {
@@ -184,8 +156,40 @@ Lightmask.prototype.initialize = function () {
     this._createBitmap();
 };
 
-Lightmask.prototype._createBitmap = function() {
-    this._maskBitmap = new Bitmap(Number(umc.Lighting.parameters['Screensize X']) + 20, Number(umc.Lighting.parameters['Screensize Y']));   // one big bitmap to fill the intire screen with black
+Lightmask.prototype.getTag = function () {
+    let note = this.note;
+    let result = note.trim();
+    return result;
+};
+
+Lightmask.prototype.ReloadMapEvents = function () {
+    //**********************fill up new map-array *************************
+    this.event_note = [];
+    this.event_id = [];
+    this.event_stacknumber = [];
+    this.event_eventcount = $gameMap.events().length;
+
+    for (let i = 0, n = this.event_eventcount; i < n; i++) {
+        if ($gameMap.events()[i]) {
+            if ($gameMap.events()[i].event()) {
+                let note = this.getTag.call($gameMap.events()[i].event());
+                let note_args = note.split(" ");
+                this.note_command = note_args.shift().toLowerCase();
+
+                if (this.note_command == "light") {
+                    this.event_note.push(note);
+                    this.event_id.push($gameMap.events()[i]._eventId);
+                    this.event_stacknumber.push(i);
+                }
+            }
+        }
+    }
+};
+
+Lightmask.prototype._createBitmap = function () {
+    let screenX = umc.Lighting.parameters['Screensize X'];
+    let screenY = umc.Lighting.parameters['Screensize Y'];
+    this._maskBitmap = new Bitmap(Number(screenX) + 20, Number(screenY));   // one big bitmap to fill the intire screen with black
 };
 
 Lightmask.prototype.update = function () {
@@ -195,42 +199,24 @@ Lightmask.prototype.update = function () {
 Lightmask.prototype._updateMask = function () {
     // Reload map events if map is changed.
     let map_id = $gameMap.mapId();
-    if (map_id != umc.Lighting.oldmap) {
-        umc.Lighting.oldmap = map_id;
-        umc.Lighting.ReloadMapEvents();
+    if (map_id != this.oldmap) {
+        this.oldmap = map_id;
+        this.ReloadMapEvents();
         player_radius = Number(umc.Lighting.parameters['Player radius']);
-        $gameVariables.SetRadius(player_radius);  
+        $gameVariables.SetRadius(player_radius);
     }
 
     // reload mapevents if event_data has chanced (deleted or spawned events/saves)
-    if (umc.Lighting.event_eventcount != $gameMap.events().length) {
-        umc.Lighting.ReloadMapEvents();
+    if (this.event_eventcount != $gameMap.events().length) {
+        this.ReloadMapEvents();
     }
 
     // remove old sprites
-    for (let i = 0, n = this._sprites.length; i < n; i++) {	
-        this._removeSprite();
+    for (let i = 0, n = this._sprites.length; i < n; i++) {
+        this.removeChild(this._sprites.pop());
     }
-    this.darkenscreen();
-    
-    if ($gameMap.mapId() >= 0) {      
-        for (let i = 0, n = umc.Lighting.event_note.length; i < n; i++) {
-            var note = umc.Lighting.event_note[i];
-            var note_args = note.split(" ");
-            umc.Lighting.note_command = note_args.shift().toLowerCase();
-            this._addSprite(-20, 0, this._maskBitmap);
-            break;
-        }  
-    };
-};
-
-umc.Lighting.distance = function (x1, y1, x2, y2) {
-    return Math.abs(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
-};
-
-Lightmask.prototype.darkenscreen = function () {
-    let maxX = Number(umc.Lighting.parameters['Screensize X']);
-    let maxY = Number(umc.Lighting.parameters['Screensize Y']);
+    let maxX = Number(umc.Lighting.parameters['Screensize X'] + 24);
+    let maxY = Number(umc.Lighting.parameters['Screensize Y'] + 24);
 
     player_radius = $gameVariables.GetRadius();
     $gameVariables.SetRadius(player_radius);
@@ -238,42 +224,41 @@ Lightmask.prototype.darkenscreen = function () {
     // ****** PLAYER LIGHTGLOBE ********
     let canvas = this._maskBitmap.canvas;
     let ctx = canvas.getContext("2d");
-    this._maskBitmap.fillRect(0, 0, maxX + 20, maxY+20, '#000000');
+    this._maskBitmap.fillRect(0, 0, maxX + 20, maxY + 20, '#000000');
     ctx.globalCompositeOperation = 'lighter';
 
     let playercolor = $gameVariables.GetPlayerColor();
     let playerbrightness = $gameVariables.GetPlayerBrightness();
     let iplayer_radius = Math.floor(player_radius);
-    
-    try{
-    if (iplayer_radius > 0) {
-        this._maskBitmap.radialgradientFillRect($gamePlayer.screenX(),$gamePlayer.screenY(), 0, iplayer_radius, playercolor, '#000000', playerbrightness);
-    }}
-    catch (e){};
+
+    try {
+        if (iplayer_radius > 0) {
+            this._maskBitmap.radialgradientFillRect($gamePlayer.screenX(), $gamePlayer.screenY(), 0, iplayer_radius, playercolor, '#000000', playerbrightness);
+        }
+    }
+    catch (e) { };
 
     // ********** OTHER LIGHTSOURCES **************
-    for (let i = 0, n = umc.Lighting.event_note.length; i < n; i++) {
-        let note = umc.Lighting.event_note[i];
+    for (let i = 0, n = this.event_note.length; i < n; i++) {
+        let note = this.event_note[i];
         let note_args = note.split(" ");
-        umc.Lighting.note_command = note_args.shift().toLowerCase();
+        this.note_command = note_args.shift().toLowerCase();
 
-        if (umc.Lighting.note_command == "light") {
-            let lightsOnRadius = Number(umc.Lighting.parameters['Lights Active Radius']);
-            let distanceApart = Math.round(umc.Lighting.distance($gamePlayer.x, $gamePlayer.y, $gameMap.events()[umc.Lighting.event_stacknumber[i]]._realX, $gameMap.events()[umc.Lighting.event_stacknumber[i]]._realY));
+        if (this.note_command == "light") {
+            let lightsOnRadius = $gameVariables.GetActiveRadius();
+            let distanceApart = Math.round(this.distance($gamePlayer.x, $gamePlayer.y, $gameMap.events()[this.event_stacknumber[i]]._realX, $gameMap.events()[this.event_stacknumber[i]]._realY));
             if (distanceApart <= lightsOnRadius) {
                 let light_radius = 1;
                 light_radius = note_args.shift();
                 if (light_radius >= 0) {
-
                     let colorvalue = note_args.shift();
                     let isValidColor = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(colorvalue);
                     if (!isValidColor) {
                         colorvalue = '#FFFFFF'
                     }
-
                     try {
-                        let lx1 = $gameMap.events()[umc.Lighting.event_stacknumber[i]].screenX();
-                        let ly1 = $gameMap.events()[umc.Lighting.event_stacknumber[i]].screenY();
+                        let lx1 = $gameMap.events()[this.event_stacknumber[i]].screenX();
+                        let ly1 = $gameMap.events()[this.event_stacknumber[i]].screenY();
                         this._maskBitmap.radialgradientFillRect(lx1, ly1 - 24, 0, light_radius, colorvalue, '#000000', 0.0);
                     }
                     catch (e) { continue };
@@ -286,12 +271,26 @@ Lightmask.prototype.darkenscreen = function () {
     ctx.globalCompositeOperation = 'lighter';
     let tint_value = $gameVariables.GetTint();
 
-    if (tint_value !== 0){
+    if (tint_value !== 0) {
         this._maskBitmap.FillRect(-20, 0, maxX + 20, maxY, tint_value);
     }
     // reset drawmode to normal
     ctx.globalCompositeOperation = 'source-over';
-}
+
+    if ($gameMap.mapId() >= 0) {
+        for (let i = 0, n = this.event_note.length; i < n; i++) {
+            var note = this.event_note[i];
+            var note_args = note.split(" ");
+            this.note_command = note_args.shift().toLowerCase();
+            this._addSprite(-20, 0, this._maskBitmap);
+            break;
+        }
+    };
+};
+
+Lightmask.prototype.distance = function (x1, y1, x2, y2) {
+    return Math.abs(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
+};
 
 /**
  * @method _addSprite
@@ -316,10 +315,6 @@ Bitmap.prototype.FillRect = function (x1, y1, x2, y2, color1) {
     context.fillRect(x1, y1, x2, y2);
     context.restore();
     this._setDirty();
-};
-
-Lightmask.prototype._removeSprite = function () {
-    this.removeChild(this._sprites.pop());
 };
 
 umc.Lighting.Spriteset_Map_prototype_createLowerLayer = Spriteset_Map.prototype.createLowerLayer;
