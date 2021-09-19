ca0e2cf01f93d0e4e60103131621d7aaa74a5005
Riley <nerdychara@gmail.com>
2021-09-19 17:09:26 -0400

    Prepare a basic Typescript definition file
    Though not all the methods are there yet; it makes it easier to find stuff.

---
+++ Community_Lighting.d.ts
@@ -0,0 +1,54 @@
+interface Bitmap {
+  radialgradientFillRect(x1: number, y1: number, innerRadius: number, outerRadius: number, color1: string, color2: String, flicker: boolean, brightness: number, direction: number): void
+
+  FillCircle(centerX: number, centerY: number, xradius: number, yradius: number, color1: String): void;
+}
+
+interface Game_Interpreter {
+
+  reload(command: String, arg: String[]);
+  tileType(command: String, args: String[]);
+}
+
+interface Game_Event {
+  getLightCycle(): String[];
+  getCLTag(): String;
+
+  incrementLightCycle(): void;
+}
+
+
+
+namespace Community {
+  interface Lighting {
+    tint(args: String[]);
+    tile(args: String[]);
+    ReloadMapEvents(): void;
+
+    ReloadTagArea(): void;
+
+    distance(): number;
+
+    name: String;
+  }
+}
+
+interface Game_Variables {
+
+  _Community_Lighting_LightTags: String[];
+  GetTileArray(): String[];
+
+  GetLightTags(): String[];
+  SetTileArray(array: String[]): void;
+
+
+  SetLightTags(array: String[]): void;
+
+  SetLightArrayId(id: String): void;
+
+  SetBlockTags(array: String[]): void;
+
+  GetBlockTags(): void;
+
+  SetPlayerColor(color: String): void;
+}
