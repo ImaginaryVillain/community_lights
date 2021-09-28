e67663ba32f6c85e6450fae32501d10abfde6589
AdriLez <70440862+AdriLez@users.noreply.github.com>
2021-09-28 17:22:20 +0200

    Removed no longer used plugin commands

--- Community_Lighting.js
+++ Community_Lighting.js
@@ -416,19 +416,6 @@ Imported[Community.Lighting.name] = true;
 * - width		width of shape
 * - height		height of shape
 *
-* effect_on_event id radius color frames
-* - id			event id
-* - radius		radius
-* - color		color
-* - frames		frames to persist
-*
-* effect_on_xy x y radius color frames
-* - x			x coord
-* - y			y coord
-* - radius		radius
-* - color		color
-* - frames		frames to persist
-*
 * --------------------------------------------------------------------------
 * Kill Switch and conditional lighting
 * --------------------------------------------------------------------------
@@ -832,7 +819,7 @@ Imported[Community.Lighting.name] = true;
     const allCommands = {
       tileblock: 'tileType', regionblock: 'tileType', tilelight: 'tileType', regionlight: 'tileType', tilefire: 'tileType', regionfire: 'tileType',
       tileglow: 'tileType', regionglow: 'tileType', tint: 'tint', daynight: 'dayNight', flashlight: 'flashLight', setfire: 'setFire', fire: 'fire', light: 'light',
-      effect_on_event: 'effectOnEvent', effect_on_xy: 'effectOnXy', script: 'scriptF', reload: 'reload', tintbattle: 'tintbattle'
+      script: 'scriptF', reload: 'reload', tintbattle: 'tintbattle'
     };
     const result = allCommands[command];
     if (result) {
@@ -920,14 +907,6 @@ Imported[Community.Lighting.name] = true;
       $gameVariables.SetScriptActive(true);
     }
     $$.fireLight(args);
-  };
-
-  Game_Interpreter.prototype.effectOnEvent = function (command, args) {
-    $$.effectOnEvent(args);
-  };
-
-  Game_Interpreter.prototype.effectOnXy = function (command, args) {
-    $$.effectXy(args);
   };
 
   Game_Interpreter.prototype.scriptF = function (command, args) {
@@ -2440,79 +2419,6 @@ Imported[Community.Lighting.name] = true;
     }
     if (args[0] === 'off') {
       $gameVariables.SetFlashlight(false);
-    }
-  };
-
-  $$.effectXy = function (args) {
-    let x1 = args[0];
-    if (x1.substring(0, 1) == '#') {
-      x1 = $gameVariables.value(Number(x1.substring(1)));
-    }
-    let y1 = args[1];
-    if (y1.substring(0, 1) == '#') {
-      y1 = $gameVariables.value(Number(y1.substring(1)));
-    }
-    let radius = args[2];
-    if (radius.substring(0, 1) == '#') {
-      radius = $gameVariables.value(Number(radius.substring(1)));
-    }
-    let color = args[3];
-    let time = args[4];
-    if (time.substring(0, 1) == '#') {
-      time = $gameVariables.value(Number(time.substring(1)));
-    }
-    let def = radius + "," + color + "," + time;
-    if (args.length >= 6) {
-      let command = args[5];
-      let ctime = args[6];
-      if (ctime.substring(0, 1) == '#') {
-        ctime = $gameVariables.value(Number(ctime.substring(1)));
-      }
-      def = def + "," + command + "," + ctime;
-    }
-  };
-
-  /**
-   * 
-   * @param {String[]} args 
-   */
-  $$.effectOnEvent = function (args) {
-    x1 = 0;
-    y1 = 0;
-    let evid = -1;
-    for (let i = 0, len = $gameMap.events().length; i < len; i++) {
-      if ($gameMap.events()[i]) {
-        evid = $gameMap.events()[i]._eventId;
-        if (evid == args[0]) {
-          x1 = $gameMap.events()[i]._realX * $gameMap.tileWidth();
-          y1 = $gameMap.events()[i]._realY * $gameMap.tileHeight();
-        }
-      }
-    }
-    // def = radius,color,duration(,keyword,speed)
-    // 0. Radius
-    // 1. Color
-    // 2. Time in Frames
-    // 3. Keyword (optional)   FADEIN FADEOUT FADEBOTH GROW SHRINK GROWSHRINK BIO
-    // 4. Fade/Grow Speed in frames
-
-    let radius = args[1];
-    if (radius.substring(0, 1) == '#') {
-      radius = $gameVariables.value(Number(radius.substring(1)));
-    }
-    let color = args[2];
-    let time = args[3];
-    if (time.substring(0, 1) == '#') {
-      time = $gameVariables.value(Number(time.substring(1)));
-    }
-    let def = radius + "," + color + "," + time;
-    if (args.length >= 5) {
-      let command = args[4];
-      let ctime = args[5];
-      if (ctime.substring(0, 1) == '#') {
-        ctime = $gameVariables.value(Number(ctime.substring(1)));
-      }
-      def = def + "," + command + "," + ctime;
     }
   };
 
